2d8189f4282b8cedc0c23a93131c290b6cdf6576
jodevsa <jodevsa@gmail.com>
2016-08-07 22:06:20 +0300

    fixed .rar  issue

--- unzip_sub_buffer.js
+++ unzip_sub_buffer.js
@@ -1,12 +1,19 @@
 var AdmZip = require('adm-zip');
 var Promise = require("bluebird");
-
+var path=require('path');
 
 function unzip_sub_buffer(data){
+
 return new Promise(function(resolve,reject){
+  var files=[]
+  // if rar neglict ..
+  if(path.extname(data.filename)=='.rar'){
+    files.push({'fileName':data.filename,'data':data.data})
+    resolve(files);
+  }
+
   try{
 var zip = new AdmZip(data.data);
-var files=[]
 zip.getEntries().forEach(function(entry) {
     var entryName = entry.entryName;
     var decompressedData = zip.readFile(entry); // decompressed buffer of the entry
